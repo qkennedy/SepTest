53310d5b96f3cc60e67800254709f170b1177ab0
JLaRue96 <jvl13@case.edu>
2017-03-24 18:13:39 -0400

    Minor changes to the STRIPS classes

--- src/P4agents/DepositAction.java
+++ src/P4agents/DepositAction.java
@@ -4,32 +4,34 @@ import edu.cwru.sepia.action.Action;
 import edu.cwru.sepia.environment.model.state.Unit.UnitView;
 
 public class DepositAction implements StripsAction {
-
-	/**
-	 * This class takes a unit that is affected, and a unit doing the action
-	 * This sets the framework for how the STRIPS Actions should be implemented
-	 * @param uID
-	 * @param tcID
-	 */
-	@Override
-	public boolean preconditionsMet(GameState state) {
-		UnitView peas = state.units.get(state.peasID);
-		Position peasPos = state.peasPos;
-		Position thPos = state.thPos;
-		double xd = Math.abs(peasPos.x - thPos.x);
-		double yd = Math.abs(peasPos.y - thPos.y);
-		return((xd <= 1 && yd <= 1) && peas.getCargoAmount()>0);
-	}
-
-	@Override
-	public GameState apply(GameState state) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Action ResultantAction(GameState state) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    
+    /**
+     * This class takes a unit that is affected, and a unit doing the action
+     * This sets the framework for how the STRIPS Actions should be implemented
+     * @param uID
+     * @param tcID
+     */
+    @Override
+    public boolean preconditionsMet(GameState state) {
+        UnitView peas = state.units.get(state.peasID);
+        Position peasPos = state.peasPos;
+        Position thPos = state.thPos;
+        double xd = Math.abs(peasPos.x - thPos.x);
+        double yd = Math.abs(peasPos.y - thPos.y);
+        return((xd <= 1 && yd <= 1) && peas.getCargoAmount()>0);
+    }
+    
+    @Override
+    public GameState apply(GameState state) {
+        // TODO Auto-generated method stub
+        //GameState newState = state.
+        UnitView townhall = state.units.get(state.townhallID);
+        
+        return null;
+    }
+    
+    @Override
+    public Action ResultantAction(GameState state) {
+        return Action.createCompoundDeposit(state.peasID, state.townhallID);
+    }
 }
--- src/P4agents/GatherWoodAction.java
+++ src/P4agents/GatherWoodAction.java
@@ -4,32 +4,31 @@ import edu.cwru.sepia.action.Action;
 import edu.cwru.sepia.environment.model.state.ResourceNode.ResourceView;
 
 public class GatherWoodAction implements StripsAction {
-	
-	public ResourceView resNode;
-	@Override
-	public GameState apply(GameState state) {
-		GameState copy = state;
-		return null;
-	}
-	
-	public boolean preconditionsMet(GameState state) {
-		Position peasPos = state.peasPos;
-		for(ResourceView resView: state.woodNodes){
-			double xd = Math.abs(peasPos.x - resView.getXPosition());
-			double yd = Math.abs(peasPos.y - resView.getYPosition());
-			if((xd <= 1 && yd <= 1) && resView.getAmountRemaining() >= 100){
-				resNode = resView;
-				return true;
-			}
-		}
-		
-		return false;
-	}
-
-	@Override
-	public Action ResultantAction(GameState state) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+    
+    public ResourceView resNode;
+    @Override
+    public GameState apply(GameState state) {
+        GameState copy = state;
+        return null;
+    }
+    
+    public boolean preconditionsMet(GameState state) {
+        Position peasPos = state.peasPos;
+        for(ResourceView resView: state.woodNodes){
+            double xd = Math.abs(peasPos.x - resView.getXPosition());
+            double yd = Math.abs(peasPos.y - resView.getYPosition());
+            if((xd <= 1 && yd <= 1) && resView.getAmountRemaining() >= 100){
+                resNode = resView;
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    @Override
+    public Action ResultantAction(GameState state) {
+        return Action.createCompoundGather(state.peasID, resNode.getID());
+    }
+    
 }
