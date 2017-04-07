bc7731c8d78ba00002daee9d9caeab27074d5dc6
JLaRue96 <jvl13@case.edu>
2017-04-07 17:16:46 -0400

    changed the stripsAction interface for the new project. Implemented GatherGoldAction for multiple peasants.

--- src/P5agents/GatherGoldAction.java
+++ src/P5agents/GatherGoldAction.java
@@ -1,42 +1,77 @@
 package P5agents;
 
+import java.util.ArrayList;
+
 import edu.cwru.sepia.action.Action;
 import edu.cwru.sepia.environment.model.state.ResourceType;
 import edu.cwru.sepia.environment.model.state.ResourceNode.ResourceView;
 
 public class GatherGoldAction implements StripsAction {
-	
-    public int pID;
-    public int resID;
     
-    public GatherGoldAction(int pID, int resID){
-    	this.pID = pID; 
-    	this.resID = resID;
+    public ArrayList<myPeasant> gathPeas;
+    public ArrayList<Integer> resIDs;
+    
+    public GatherGoldAction(ArrayList<myPeasant> gathPeas, ArrayList<Integer> resIDs){
+        this.gathPeas = gathPeas;
+        this.resIDs = resIDs;
     }
     @Override
     public GameState apply(GameState state) {
-    	GameState copy = state;
-        copy.gatherFromNode(pID, resID, ResourceType.GOLD);
+        GameState copy = state;
+        
+        for(myPeasant peas : gathPeas) {
+            int index = gathPeas.indexOf(peas);
+            int resID = resIDs.get(index);
+            
+            copy.gatherFromNode(peas.getID(), resID, ResourceType.GOLD);
+        }
+        
         copy.actions.push(this);
-        //TODO Add Cost Changes
         return copy;
     }
     
     public boolean preconditionsMet(GameState state) {
-        Position peasPos = state.peasPos;
-        ResourceView view = state.getResource(resID);
+        
+        if(!sameSize()) {
+            return false;
+        }
+        
+        for(myPeasant peas : gathPeas) {
+            int index = gathPeas.indexOf(peas);
+            
+            //myPeasant peasant = state.peasants.get(id);
+            Position peasPos = peas.getPos();
+            int cargo = peas.getCAmt();
+            ResourceView view = state.getResource(resIDs.get(index));
             double xd = Math.abs(peasPos.x - view.getXPosition());
             double yd = Math.abs(peasPos.y - view.getYPosition());
-            boolean emptyHold = state.pAmt == 0;
-            return ((xd <= 1 && yd <= 1) && view.getAmountRemaining() >= 100) && emptyHold;
+            boolean emptyHold = cargo == 0;
+            boolean precondition = ((xd <= 1 && yd <= 1) && view.getAmountRemaining() >= 100) && emptyHold;
+            
+            if(!precondition) {
+                return false;
+            }
+        }
+        
+        return true;
+        
+    }
+    
+    public boolean sameSize() {
+        return (gathPeas.size() == resIDs.size());
     }
     
     @Override
-    public Action ResultantAction() {
-        return Action.createCompoundGather(pID, resID);
+    public ArrayList<Action> ResultantAction() {
+        
+        ArrayList<Action> actionList = new ArrayList<Action>();
+        for(myPeasant peas : gathPeas) {
+            int index = gathPeas.indexOf(peas);
+            int resID = resIDs.get(index);
+            actionList.add(Action.createCompoundGather(peas.getID(), resID));
+        }
+        return actionList;
+        
     }
-	@Override
-	public int getPID() {
-		return pID;
-	}
+    
 }
